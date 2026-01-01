0ff4cd84c3cd2a16aba65b8eacf8f73f759524ee
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2026-01-01 10:24:54 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,21 @@ export default function Home() {
     navigate(`/tracker/${user.toLowerCase()}`);
   };
 
+  const handleResultsClick = () => {
+    navigate('/results');
+  }
+
   return (
     <div className="space-y-4 h-screen flex flex-col items-center justify-baseline text-white">
-      <h1 className="text-3xl font-bold my-8">Welcome! Who is this?</h1>
-      <button
+      <h1 className="text-3xl font-bold my-8 mx-8">The challenge has concluded! See final results:</h1>
+      <button 
+        className="bg-jl-red w-3/4 md:w-1/2 lg:w-1/3 h-1/4 py-4 text-4xl md:text-5xl lg:text-6xl font-semibold hover:bg-jl-red_hover cursor-pointer rounded-lg shadow-lg"
+        onClick={() => handleResultsClick()}
+      >
+        Final results
+      </button>
+      {/* <h1 className="text-3xl font-bold my-8">Welcome! Who is this?</h1> */}
+      {/* <button
         className="bg-jl-red w-full h-1/3 py-4 text-6xl font-semibold hover:bg-jl-red_hover cursor-pointer"
         onClick={() => handleSelect("jason")}
       >
@@ -21,7 +32,7 @@ export default function Home() {
         onClick={() => handleSelect("gabby")}
       >
         Gabby
-      </button>
+      </button> */}
     </div>
   );
 }
